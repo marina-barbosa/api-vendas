af2e34567aac8e8eb05a50a1947502b55bc2e18e
marina-barbosa <marinabarbosa.exp@gmail.com>
2023-12-24 01:10:30 -0300

    feat: add users repositories

---
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,36 @@
+import { appDataSource } from '@shared/typeorm';
+import User from '../entities/User';
+
+export const ProductRepository = appDataSource.getRepository(User).extend({
+
+  async findByName(name: string): Promise<User | null> {
+
+    const user = await this.findOne({
+      where: {
+        name,
+      },
+    });
+    return user;
+  },
+
+  async findById(id: string): Promise<User | null> {
+
+    const user = await this.findOne({
+      where: {
+        id,
+      },
+    });
+    return user;
+  },
+
+  async findByEmail(email: string): Promise<User | null> {
+
+    const user = await this.findOne({
+      where: {
+        email,
+      },
+    });
+    return user;
+  },
+
+})
