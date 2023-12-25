10b27d0b6da85a78c81fd29af84cd5d4f6bba287
marina-barbosa <marinabarbosa.exp@gmail.com>
2023-12-25 05:01:09 -0300

    feat: controller e router reset password

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
+import ResetPasswordController from '../controllers/ResetPasswordController';
 
 
 const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
+const resetPasswordController = new ResetPasswordController();
 
 passwordRouter.post(
   '/forgot',            // localhost:3333/password/forgot
@@ -14,6 +16,18 @@ passwordRouter.post(
     }
   }),
   forgotPasswordController.create,
+);
+
+passwordRouter.post(
+  '/reset',            // localhost:3333/password/reset
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().required().uuid(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    }
+  }),
+  resetPasswordController.create,
 );
 
 export default passwordRouter;
