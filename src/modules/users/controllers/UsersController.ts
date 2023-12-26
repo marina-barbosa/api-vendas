cceb7a24e7731a1bd5dad46dd7166437469ce365
marina-barbosa <marinabarbosa.exp@gmail.com>
2023-12-26 08:33:13 -0300

    feat: config exclude password do class-transfomer, no user e user controller

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import ListUserService from '../services/ListUserService';
 import CreateUserService from '../services/CreateUserService';
+import { instanceToInstance } from 'class-transformer'
 
 export default class UsersController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -8,7 +9,7 @@ export default class UsersController {
 
     const users = await listUser.execute();
 
-    return response.json(users);
+    return response.json(instanceToInstance(users));
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
